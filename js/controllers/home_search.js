686e0b19555d25283ccaeb32e951d218d366f123
Nicolas Andruzzi <NicolasAndruzzi@gmail.com>
2016-02-26 03:26:53 -0700

    app styled

--- js/controllers/home_search.js
+++ js/controllers/home_search.js
@@ -1,11 +1,8 @@
 app.controller('HomeSearchController', function ($scope, $http) {
   if ($scope.searchBox == undefined) {
     $scope.searchBox = ""
-  }
+  };
     $http.get('http://www.omdbapi.com/?s=' + $scope.searchBox).then(function(data){
       $scope.apiReturn = data.data.Search;
-      // console.log("HomeSearchController");
-      // console.log($scope.apiReturn[0].imdbID)
-      // console.log("**************");
     });
 });
